001c55bc9cb83a85cc740d2023a23ab057250bf4
Nicole Schmidt <nicoleschmidt2000@gmail.com>
2022-12-12 13:10:34 -0300

    arruma nome da variavel e adiciona estrutura para calculo da matriz de custo

--- algoritmos/tu2017/similarity.py
+++ algoritmos/tu2017/similarity.py
@@ -1,9 +1,12 @@
 import numpy as np
-from datetime import timedelta,datetime
+from .treatData import PoiCategory, TuTrajectory, TuPoint
+from datetime import timedelta, datetime, timezone
 from geopy.distance import geodesic
 
 #10 and 0.5 are thresholds
-def msm(trajectory_a,trajectory_b):
+# preciso entender o que esse método faz e como faz
+# eu sei que ele ve a similaridade entre duas trajetorias
+def msm(trajectory_a: TuTrajectory, trajectory_b: TuTrajectory):
     results = []
     for point_a in trajectory_a.trajectory:
         line = []
@@ -39,7 +42,7 @@ def distance(a,b):
     return geodesic(coordinate_one,coordinate_two).miles
 
 #time dimension
-def time(a,b):
+def time(a: TuPoint, b:TuPoint) -> float:
     tempo2_a = a.utc_timestamp + a.duration
     tempo2_b = b.utc_timestamp + b.duration
     if tempo2_a < b.utc_timestamp or tempo2_b < a.utc_timestamp:
@@ -58,3 +61,94 @@ def semantics(a,b):
     if a.venue_category_id.intersection(b.venue_category_id):
         return 1
     return 0
+
+
+def create_cost_matrix(trajectories: list[TuTrajectory], weights: dict[str, int]):
+    """
+    Cria a matriz de custo C.
+    Como Cij == Cji a matriz é preenchida somente na parte superior
+    e a parte inferior é copiada da superior já existente
+    """
+    matrix = []
+    for i, trajectory_one in enumerate(trajectories):
+        lista = []
+
+        for j in range(i):
+            lista.append(matrix[j][i])
+
+        for j, trajectory_two in enumerate(trajectories[i:]):
+            if trajectory_one == trajectory_two:
+                lista.append(float('inf'))
+            else:
+                cost = spatio_temporal_loss(trajectory_one, trajectory_two, weights)
+                # lista.append(msm(trajectory_one,trajectory_two)) # -> fazer
+                lista.append(cost)
+        matrix.append(lista)
+    return matrix
+
+
+def spatio_temporal_loss(trajectory_a: TuTrajectory, trajectory_b: TuTrajectory, weights: dict[str, int]):
+    """
+    Calcula a perda espaço-temporal entre duas trajetórias
+    """
+
+    pass
+
+
+def point_loss(point_a: TuPoint, point_b: TuPoint, n_a: int, n_b: int):
+    """
+    Args: n_a e n_b são a quantidade de trajetórias presentes na trajetória recebida anteriormente, é o parametro n
+    Calcula a perda espaço-temporal da junção de dois pontos
+    **** lembrete que preciso considerar que se a diferença de tempo entre dois pontos for > 8 horas a perda é total
+    """
+
+    pass
+
+
+def calculate_new_duration(point_a: datetime, point_b: datetime) -> datetime:
+    """
+    Calcula o novo intervalo de tempo que iria resultar da junção de dois pontos
+    Considera-se que a diferença de tempo entre os dois pontos é menor que 8 horas -> acho que a diferença de tempo poderia ser menor, tipo 2/3 horas
+    """
+
+    if point_a <= point_b:
+        return point_b
+
+    return point_a
+
+
+
+def temporal_loss(point_a: TuPoint, point_b: TuPoint, n_a: int, n_b: int):
+    """
+    Calcula a perda temporal da junção de dois pontos
+    """
+
+    a_initPoint = point_a.utc_timestamp
+    a_endPoint = a_initPoint + point_a.duration
+    b_initPoint = point_b.utc_timestamp
+    b_endPoint = b_initPoint + point_b.duration
+
+    duration_c = calculate_new_duration(a_endPoint, b_endPoint)
+
+    teta_star = ((duration_c - a_endPoint) * n_a + (duration_c - b_endPoint) * n_b) / (n_a + n_b)
+
+    return min(teta_star, 1)
+
+
+
+
+def spatial_loss(point_a: TuPoint, point_b: TuPoint, n_a: int, n_b: int):
+    pass
+
+
+if __name__ == "__main__":
+    a = TuPoint(name='10', user_id='1', category={PoiCategory.Business}, latitude=0.0, longitude=0.0, utc_timestamp=datetime(2012, 4, 3, 18, 20, 0, tzinfo=timezone.utc), duration=timedelta(seconds=3600))
+    b = TuPoint(name='12', user_id='2', category={PoiCategory.Transport}, latitude=9.0, longitude=8.0, utc_timestamp=datetime(2012, 4, 3, 19, 30, tzinfo=timezone.utc), duration=timedelta(0))
+
+    a_initPoint = a.utc_timestamp
+    a_endPoint = a_initPoint + a.duration
+    b_initPoint = b.utc_timestamp
+    b_endPoint = b_initPoint + b.duration
+
+    d = calculate_new_duration(a_endPoint, b_endPoint)
+    # print(d)
--- algoritmos/tu2017/treatData.py
+++ algoritmos/tu2017/treatData.py
@@ -1,7 +1,8 @@
-import similarity
-
 from ..utils.trajetoria import process_trajectories, Trajectory, Point
+from dataclasses import dataclass
+from datetime import timedelta, datetime
 from enum import Enum, auto
+from typing import List, Set
 
 # t-closeness: distribuição igual de PoI
 # l-diversity: every point has at least l categories
@@ -26,7 +27,7 @@ class TuPoint:
 
 @dataclass
 class TuTrajectory:
-    trajectory: List[Point]
+    trajectory: List[TuPoint]
     n: int = 1
 
 def get_venue_category(trajectories: list[Trajectory]) -> list[TuTrajectory]:
@@ -46,7 +47,7 @@ def modify_point(point: Point) -> TuPoint:
     return TuPoint(
         point.name,
         point.user_id,
-        generalize_venue_category(point.venue_category),
+        {generalize_venue_category(point.venue_category)},
         point.latitude,
         point.longitude,
         point.utc_timestamp,
@@ -54,42 +55,26 @@ def modify_point(point: Point) -> TuPoint:
 
 
 def generalize_venue_category(venue_category: str) -> PoiCategory:
-    if any(sub_str in categoria for sub_str in ['Shop', 'Store', 'Restaurant', 'Bakery', 'Wash', 'Embassy', 'Ramen', 'Diner', 'Salon', 'Place', 'Steakhouse', 'Market', 'Joint', 'store', 'Food', 'Service', 'Bar', 'Café', 'Cafe', 'Office', 'Bank', 'Mall', 'Newsstand', 'Fair', 'Tea', 'City', 'Gastropub', 'Studio', 'Bodega', 'Rental', 'Dealership', 'Photography Lab', 'Medical Center']):
+    if any(sub_str in venue_category for sub_str in ['Shop', 'Store', 'Restaurant', 'Bakery', 'Wash', 'Embassy', 'Ramen', 'Diner', 'Salon', 'Place', 'Steakhouse', 'Market', 'Joint', 'store', 'Food', 'Service', 'Bar', 'Café', 'Cafe', 'Office', 'Bank', 'Mall', 'Newsstand', 'Fair', 'Tea', 'City', 'Gastropub', 'Studio', 'Bodega', 'Rental', 'Dealership', 'Photography Lab', 'Medical Center']):
         return PoiCategory.Business
-    elif any(sub_str in categoria for sub_str in ['Factory', 'Military', 'Distillery', 'Government', 'Harbor', 'Facility', 'Winery', 'Brewery']):
+    elif any(sub_str in venue_category for sub_str in ['Factory', 'Military', 'Distillery', 'Government', 'Harbor', 'Facility', 'Winery', 'Brewery']):
         return PoiCategory.Industry
-    elif any(sub_str in categoria for sub_str in ['Residential Building', 'Building', 'Shelter', 'Neighborhood', 'Home', 'Hotel', 'Housing', 'House']):
+    elif any(sub_str in venue_category for sub_str in ['Residential Building', 'Building', 'Shelter', 'Neighborhood', 'Home', 'Hotel', 'Housing', 'House']):
         return PoiCategory.Residence
-    elif any(sub_str in categoria for sub_str in ['Scenery', 'Scenic', 'Park', 'Outdoors', 'Garden', 'Museum', 'Castle', 'River', 'Cemetery', 'Temple', 'Synagogue', 'Church', 'Shrine', 'Historic', 'Mosque', 'Planetarium', 'Spot', 'Rest Area', 'Plaza', 'Spiritual', 'Campground']):
+    elif any(sub_str in venue_category for sub_str in ['Scenery', 'Scenic', 'Park', 'Outdoors', 'Garden', 'Museum', 'Castle', 'River', 'Cemetery', 'Temple', 'Synagogue', 'Church', 'Shrine', 'Historic', 'Mosque', 'Planetarium', 'Spot', 'Rest Area', 'Plaza', 'Spiritual', 'Campground']):
         return PoiCategory.Scenery
-    elif any(sub_str in categoria for sub_str in ['School', 'College', 'Student', 'University']):
+    elif any(sub_str in venue_category for sub_str in ['School', 'College', 'Student', 'University']):
         return PoiCategory.Education
-    elif any(sub_str in categoria for sub_str in ['Music', 'Movie', 'Playground', 'Arcade', 'Art', 'Entertainment', 'Gym', 'Nightlife', 'Spa', 'Pool', 'Library', 'Aquarium', 'Beach', 'Zoo', 'Bowling', 'Theater', 'Athletic', 'Casino', 'Comedy', 'Stadium', 'Concert', 'Convention', 'Ski', 'Racetrack']):
+    elif any(sub_str in venue_category for sub_str in ['Music', 'Movie', 'Playground', 'Arcade', 'Art', 'Entertainment', 'Gym', 'Nightlife', 'Spa', 'Pool', 'Library', 'Aquarium', 'Beach', 'Zoo', 'Bowling', 'Theater', 'Athletic', 'Casino', 'Comedy', 'Stadium', 'Concert', 'Convention', 'Ski', 'Racetrack']):
         return PoiCategory.Entertainment
-    elif any(sub_str in categoria for sub_str in ['Train', 'Bike', 'Airport', 'Ferry', 'Station', 'Road', 'Moving', 'Transport', 'Subway', 'Bridge', 'Travel', 'Taxi', 'Light Rail']):
+    elif any(sub_str in venue_category for sub_str in ['Train', 'Bike', 'Airport', 'Ferry', 'Station', 'Road', 'Moving', 'Transport', 'Subway', 'Bridge', 'Travel', 'Taxi', 'Light Rail']):
         return PoiCategory.Transport
 
 
-def create_similarity_matrix(trajectories: list[TuTrajectory]):
-    matrix = []
-    for i, trajectory_one in enumerate(trajectories):
-        lista = []
-
-        for j in range(i):
-            lista.append(matrix[j][i])
-
-        for j, trajectory_two in enumerate(trajectories[i:]):
-            if trajectory_one == trajectory_two:
-                lista.append(float('-inf'))
-            else:
-                lista.append(similarity.msm(trajectory_one,trajectory_two))
-        matrix.append(lista)
-    return matrix
-
-
+def construct_graph(trajectories: list[TuTrajectory]):
+    pass
 
 if __name__ == "__main__":
-    # lista_categorias = ['College Stadium', 'Sorority House', 'Train Station', 'Mosque', 'Music Venue', 'Nail Salon', 'Subway', 'Internet Cafe', 'Moving Target', 'Residential Building (Apartment / Condo)', 'Bar', 'Mac & Cheese Joint', 'Athletic & Sport', 'Gastropub', 'Building', 'Factory', 'Convention Center', 'Scenic Lookout', 'Professional & Other Places', 'Indian Restaurant', 'Bike Shop', 'Castle', 'Housing Development', 'Mexican Restaurant', 'Event Space', 'Deli / Bodega', 'Casino', 'Beer Garden', 'Church', 'General Travel', 'Ethiopian Restaurant', 'Cosmetics Shop', 'Peruvian Restaurant', 'River', 'Music School', 'School', 'Malaysian Restaurant', 'Comedy Club', 'Road', 'Car Dealership', 'Garden Center', 'Burrito Place', 'South American Restaurant', 'Camera Store', 'Post Office', 'Synagogue', 'Elementary School', 'Law School', 'Latin American Restaurant', 'Bridge', 'Convenience Store', 'Tea Room', 'Rest Area', 'Food Truck', 'Recycling Facility', 'Nursery School', 'Bank', 'Travel Lounge', 'Rental Car Location', 'Animal Shelter', 'College Academic Building', 'Taxi', 'Fair', 'Sculpture Garden', 'Government Building', 'Trade School', 'Harbor / Marina', 'Seafood Restaurant', 'Spanish Restaurant', 'Fraternity House', 'Moroccan Restaurant', 'Historic Site', 'Cajun / Creole Restaurant', 'Temple', 'Miscellaneous Shop', 'Eastern European Restaurant', 'Japanese Restaurant', 'Embassy / Consulate', 'Automotive Shop', 'Funeral Home', 'BBQ Joint', 'Fried Chicken Joint', 'Fish & Chips Shop', 'Italian Restaurant', 'American Restaurant', 'Distillery', 'Australian Restaurant', 'University', 'Flower Shop', 'Food', 'Toy / Game Store', 'Other Great Outdoors', 'Swiss Restaurant', 'Clothing Store', 'Sporting Goods Shop', 'Gaming Cafe', 'Campground', 'Food & Drink Shop', 'Shrine', 'Stadium', 'College & University', 'Spiritual Center', 'Bagel Shop', 'Brazilian Restaurant', 'Cemetery', 'Falafel Restaurant', 'Parking', 'Gift Shop', 'Asian Restaurant', 'Record Shop', 'Bookstore', 'Garden', 'Performing Arts Venue', 'Racetrack', 'Gym / Fitness Center', 'Bakery', 'Museum', 'Diner', 'Design Studio', 'Travel & Transport', 'Laundry Service', 'Sushi Restaurant', 'Hotel', 'Vegetarian / Vegan Restaurant', 'Arts & Crafts Store', 'Hobby Shop', 'Antique Shop', 'German Restaurant', 'Plaza', 'Hot Dog Joint', 'Middle School', 'African Restaurant', 'General College & University', 'Community College', 'Gas Station / Garage', 'Spa / Massage', 'Southern / Soul Food Restaurant', 'Afghan Restaurant', 'Pool', 'Video Store', 'Vietnamese Restaurant', 'Taco Place', 'Mobile Phone Shop', 'Donut Shop', 'College Theater', 'Scandinavian Restaurant', 'Chinese Restaurant', 'Tanning Salon', 'Art Museum', 'Pizza Place', 'Video Game Store', 'Pet Service', 'Fast Food Restaurant', 'Theater', 'Military Base', 'City', 'Drugstore / Pharmacy', 'Sandwich Place', 'Science Museum', 'Ramen /  Noodle House', 'Outdoors & Recreation', 'Caribbean Restaurant', 'Restaurant', 'Bowling Alley', 'Student Center', 'Light Rail', 'Bridal Shop', 'Gluten-free Restaurant', 'General Entertainment', 'Dumpling Restaurant', 'Newsstand', 'Breakfast Spot', 'Wings Joint', 'Candy Store', 'Furniture / Home Store', 'Cupcake Shop', 'Shop & Service', 'Photography Lab', 'Smoke Shop', 'Soup Place', 'Salon / Barbershop', 'Paper / Office Supplies Store', 'Brewery', 'Bike Rental / Bike Share', 'Library', 'Office', 'Motorcycle Shop', 'Bus Station', 'Department Store', 'Neighborhood', 'Mall', 'Burger Joint', 'Electronics Store', 'Ski Area', 'Steakhouse', 'Thai Restaurant', 'Café', 'Korean Restaurant', 'Public Art', 'Medical School', 'Ferry', 'Airport', 'Thrift / Vintage Store', 'Storage Facility', 'Flea Market', 'History Museum', 'Tapas Restaurant', 'Park', 'Salad Place', 'Snack Place', 'Home (private)', 'Planetarium', 'Winery', 'Playground', 'Movie Theater', 'Arcade', 'Dim Sum Restaurant', 'Portuguese Restaurant', 'Nightlife Spot', 'Arts & Entertainment', 'Art Gallery', 'Market', 'Aquarium', 'Concert Hall', 'Beach', 'Coffee Shop', 'Other Nightlife', 'Medical Center', 'Zoo', 'Car Wash', 'Ice Cream Shop', 'Financial or Legal Service', 'Cuban Restaurant', 'High School', 'Pool Hall']
     count = 0
     # for categoria in lista_categorias:
     #     a = generalize_venue_category(categoria)
