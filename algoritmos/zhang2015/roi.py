c6383a29237a558268c099f1ca0016324739a059
Nico Schmidt <n.schmidt@grad.ufsc.br>
2023-08-28 17:05:39 -0300

    Calcula os vizinhos de um PoI antes de começar a formar os agrupamentos

--- algoritmos/zhang2015/roi.py
+++ algoritmos/zhang2015/roi.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
+from datetime import timedelta
 
-from algoritmos.utils.math_utils import distance
+from algoritmos.utils.math_utils import distance, time_difference
 from algoritmos.zhang2015.poi import PoI
 
 
@@ -21,28 +22,25 @@ class Clusters:
         pass
 
 
-def cluster_poi(points: set[PoI], spatial_radius: float, temporal_radius, density_threshold) -> Clusters:
+def cluster_poi(points: set[PoI], spatial_radius: float, temporal_radius: timedelta, density_threshold) -> Clusters:
+    calculate_neighbours(points, spatial_radius, temporal_radius)
+
     clusters = []
-    unvisited_points = [point for point in points]
+    visited = []
     noise_candidate = []
     for point in points:
-        if point in unvisited_points:
-            unvisited_points.remove(point)
-            neighbours = get_neighbours(point, points, spatial_radius, temporal_radius)
-            if len(neighbours) < density_threshold:
+        if point not in visited:
+            visited.append(point)
+            if len(point.neighbours) < density_threshold:
                 noise_candidate.append(point)
             else:
-                # TODO: pedir a opinião do tiz de como lidar com esse unvisited
-                #  points, imagino que retornar ele assim n seja legal
-                unvisited_points, cluster = expand_cluster(point, neighbours, clusters, spatial_radius, temporal_radius,
-                                                           density_threshold, unvisited_points)
+                cluster = expand_cluster(point, clusters, density_threshold, visited)
                 clusters.append(cluster)
 
     final_clusters = Clusters(clusters=clusters)
     noise = []
     for candidate in noise_candidate:
-        ncN = get_neighbours(candidate, points, spatial_radius, temporal_radius)
-        if len(ncN) > 0:
+        if len(candidate.neighbours) > 0:
             final_clusters.add_to_nearest_cluster(candidate)
         else:
             noise.append(candidate)
@@ -51,37 +49,46 @@ def cluster_poi(points: set[PoI], spatial_radius: float, temporal_radius, densit
 
 
 # Algorithm 3, Pag 5
-def expand_cluster(point: PoI, neighbours: list[PoI], clusters: list[RoI], spatial_radius: float, temporal_radius, density_threshold, unvisited):
+def expand_cluster(point: PoI, clusters: list[RoI], density_threshold, visited):
     roi = RoI(
         core=point,
         points=[point]
     )
 
-    for neighbour in neighbours:
-        if neighbour in unvisited:
-            unvisited.remove(neighbour)
-            M = get_neighbours(neighbour, spatial_radius, temporal_radius)
-            if len(M) >= density_threshold:
-                neighbours |= M  # TODO: ver com o tiz se essa alteração na lista enquanto ela é iterada é ok
+    neighbours = point.neighbours
+    while True:
+        new_neighbours = []
+        for neighbour in neighbours:
+            if neighbour not in visited:
+                visited.append(neighbour)
+                if len(neighbour.neighbours) >= density_threshold:
+                    new_neighbours.extend(neighbour.neighbours)
 
-        for created_clusters in clusters:
-            if not created_clusters.belongs(neighbour):
+            clustered = False
+            for created_clusters in clusters:
+                if created_clusters.belongs(neighbour):
+                    clustered = True
+
+            if not clustered:
                 roi.points.append(neighbour)
 
-    return unvisited, roi
+        if not new_neighbours:
+            break
+
+        neighbours = new_neighbours
+
+    return roi
 
 
 # A call of Retrieve_Neighbors(object, Eps1, Eps2) returns the objects that have a distance less
 # than Eps1 and Eps2 parameters to the selected object. In other words, Retrieve_Neighbors function retrieves
 # all objects density-reachable (see Definition 6) from the selected object with respect to Eps1, Eps2, and MinPts
-def get_neighbours(point: PoI, points: set[PoI], spatial_radius, temporal_radius) -> list[PoI]:
-    neighbours = []
-
-    for poi in points:
-        if poi == point:
-            continue
-
-        if distance(point.loc, poi.loc) <= spatial_radius and (poi.t - point.t) <= temporal_radius:
-            neighbours.append(poi)
-
-    return neighbours
+def calculate_neighbours(points: set[PoI], spatial_radius: float, temporal_radius: timedelta) -> None:
+    """
+    Calcula os vizinhos para cada PoI e atualiza a sua lista.
+    """
+    for i, point in enumerate(points):
+        for candidate in points[i:]:
+            if distance(point.loc, candidate.loc) <= spatial_radius and time_difference(candidate.t, point.t) <= temporal_radius:
+                point.neighbours.append(candidate)
+                candidate.neighbours.append(point)
