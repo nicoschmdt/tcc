b951cb02266fc6f638ac973bd6811414568c2bc1
Nico Schmidt <n.schmidt@grad.ufsc.br>
2023-09-05 14:31:18 -0300

    Altera funções argmin e argmax invocadas

--- algoritmos/tu2017/tu.py
+++ algoritmos/tu2017/tu.py
@@ -1,38 +1,30 @@
-from pprint import pprint
-from numpy import argmax, argmin
-
 from algoritmos.tu2017 import cost_matrix
+from algoritmos.tu2017.cost_matrix import create_cost_matrix
 from algoritmos.tu2017.treat_data import construct_graph, TuPoint, TuTrajectory
 from algoritmos.utils import region
 from algoritmos.utils.graph import get_connected_region, Graph
+from algoritmos.utils.semantic import SemanticTrajectory
 
 
-def merging_trajectories(trajectories: list[TuTrajectory],
-                         merge_cost_matrix: dict[TuTrajectory, list[tuple[float, TuTrajectory]]],
-                         delta_k: int, delta_l: int, delta_t: float):
-    """
-    """
-    graph = construct_graph(trajectories)
+def merging_trajectories(trajectories: list[SemanticTrajectory], delta_k: int, delta_l: int, delta_t: float):
+    graph, tu_trajectories = construct_graph(trajectories)
+    merge_cost_matrix = create_cost_matrix(tu_trajectories)
     generalized_dataset = []
 
-    while len(trajectories) > 2:
-
-        # ver como isso vai ser pego
-        i, j = argmin(merge_cost_matrix)
-        first_trajectory = trajectories[i]
-        second_trajectory = trajectories[j]
+    while len(tu_trajectories) > 2:
+        first_trajectory, second_trajectory = cost_matrix.get_minimal_trajectory_merge(merge_cost_matrix)
 
         # new trajectory
-        tm = merge(trajectories[i], trajectories[j], graph, delta_l, delta_t)
+        tm = merge(first_trajectory, second_trajectory, graph, delta_l, delta_t)
 
-        trajectories.remove(first_trajectory)
-        trajectories.remove(second_trajectory)
-        merge_cost_matrix = cost_matrix.remove_from_cost_matrix(merge_cost_matrix, i)
-        merge_cost_matrix = cost_matrix.remove_from_cost_matrix(merge_cost_matrix, j)
+        tu_trajectories.remove(first_trajectory)
+        tu_trajectories.remove(second_trajectory)
+        merge_cost_matrix = cost_matrix.remove_from_cost_matrix(merge_cost_matrix, first_trajectory)
+        merge_cost_matrix = cost_matrix.remove_from_cost_matrix(merge_cost_matrix, second_trajectory)
 
         if tm.n < delta_k:
             merge_cost_matrix = cost_matrix.add_trajectory_cost(merge_cost_matrix, tm)
-            trajectories.append(tm)
+            tu_trajectories.append(tm)
         else:
             generalized_dataset.append(tm)
 
@@ -44,7 +36,7 @@ def merge(trajectory: TuTrajectory, trajectory2: TuTrajectory, graph: Graph, del
     """
     Identifica qual das duas trajetórias possui a maior quantidade de pontos.
     """
-    if len(trajectory.trajectory) > len(trajectory2.trajectory):
+    if len(trajectory.points) > len(trajectory2.points):
         return merge_trajectories(trajectory, trajectory2, graph, delta_l, delta_t)
     return merge_trajectories(trajectory2, trajectory, graph, delta_l, delta_t)
 
@@ -53,17 +45,17 @@ def merge_trajectories(bigger_trajectory: TuTrajectory, smaller_trajectory: TuTr
                        delta_l: int, delta_t: float) -> TuTrajectory:
     """
     """
-    points = [(index, False) for index, _ in enumerate(smaller_trajectory.trajectory)]
+    points = [(index, False) for index, _ in enumerate(smaller_trajectory.points)]
 
-    for point in bigger_trajectory.trajectory:
-        chosen_point, _ = get_smallest_merge_cost_partner(point, smaller_trajectory.trajectory)
+    for point in bigger_trajectory.points:
+        chosen_point, _ = get_smallest_merge_cost_partner(point, smaller_trajectory.points)
         merged_point, index = merge_points(point, chosen_point, graph, delta_l, delta_t)
-        smaller_trajectory.trajectory[index] = merged_point
+        smaller_trajectory.points[index] = merged_point
         points[index] = (index, True)
 
     altered_points = merge_remaining_points(smaller_trajectory, points, graph, delta_l, delta_t)
 
-    semantic_trajectory = TuTrajectory(trajectory=altered_points, n=bigger_trajectory.n + smaller_trajectory.n)
+    semantic_trajectory = TuTrajectory(points=altered_points, n=bigger_trajectory.n + smaller_trajectory.n)
     semantic_trajectory.reshape()
     return semantic_trajectory
 
@@ -73,10 +65,10 @@ def merge_remaining_points(trajectory: TuTrajectory, point_status: list[tuple[in
     Une os pontos que ainda não foram alterados da menor trajetória
     com o ponto com o menor custo de junção de si mesmo.
     """
-    altered_points = trajectory.trajectory.copy()
+    altered_points = trajectory.points.copy()
     for index, merged in point_status:
         if not merged:
-            actual_point = trajectory.trajectory[index]
+            actual_point = trajectory.points[index]
             chosen_point, point_index = get_smallest_merge_cost_partner(actual_point, altered_points, trajectory.n)
             merge_point, _ = merge_points(actual_point, chosen_point, graph, delta_l, delta_t)
             altered_points[index] = merge_point
@@ -115,32 +107,19 @@ def merge_points(point_a: TuPoint, point_b: TuPoint, graph: Graph, delta_l: floa
     while lc.get_diversity() < delta_l:
         diversities = {}
         for neighbour in lc.neighbours:
-            diversity = region.get_possible_diversity(lc, neighbour)
-            diversities[neighbour] = diversity
+            diversities[neighbour] = region.get_possible_diversity(lc, neighbour)
 
-        lc.join_region(argmax(diversities))
+        lc.join_region(max(diversities, key=diversities.get))
 
     while lc.get_closeness(graph.poi_distribution) < delta_t:
-        closeness_values = {}
+        closeness = {}
         for neighbour in lc.neighbours:
-            closeness = region.get_possible_closeness(lc, neighbour, graph.poi_distribution)
-            closeness_values[neighbour] = closeness
+            closeness[neighbour] = region.get_possible_closeness(lc, neighbour, graph.poi_distribution)
 
-        lc.join_region(argmin(closeness_values))
+        lc.join_region(min(closeness, key=closeness.get))
 
     return TuPoint(
         utc_timestamp=tc,
         duration=duration,
         region=lc
     )
-
-
-def main(name, anonymity_criteria):
-    pass
-    # trajectories = process_trajectories(name)
-    # similarity_matrix = create_similarity_matrix(trajectories)
-    # anonymized = merge_trajectories(trajectories,similarity_matrix,anonymity_criteria)
-
-
-if __name__ == '__main__':
-    main('resources/dataset_TSMC2014_TKY.csv', 3)
