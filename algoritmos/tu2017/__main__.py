58e3a4fef2f0c5906a7dc0998b29ac49439977a0
Nico Schmidt <nicoschmdt@proton.me>
2023-10-13 15:38:00 -0300

    Atualiza import do main

--- algoritmos/tu2017/__main__.py
+++ algoritmos/tu2017/__main__.py
@@ -1,4 +1,4 @@
-from .cli import main
+from algoritmos.tu2017.cli import main
 
 if __name__ == '__main__':
     main()
--- algoritmos/zhang2015/__main__.py
+++ algoritmos/zhang2015/__main__.py
@@ -1,4 +1,4 @@
-from .cli import main
+from algoritmos.zhang2015.cli import main
 
 if __name__ == '__main__':
     main()
